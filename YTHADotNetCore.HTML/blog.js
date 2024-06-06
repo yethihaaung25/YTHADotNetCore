e8224171a12292b74c2f8c9cadefdd4702e8e05a
Ace <yethihaaung.technical.mc@gmail.com>
2024-06-07 02:34:31 +0630

    [Complete] BlogCRUDwithUI

--- YTHADotNetCore.HTML/blog.js
+++ YTHADotNetCore.HTML/blog.js
@@ -1,42 +1,54 @@
 const tblBlog = "blogs";
+let blogId = null;
+getBlogTable();
 // CreateBlog();
 // UpdateBlog("bf3b70dc-da0c-4fff-b7d3-b11baf272064","Hello","Ye","Love")
-DeleteBlog("223fd028-a85d-4af3-bb1b-dcc6dc5cd51b")
+// DeleteBlog("223fd028-a85d-4af3-bb1b-dcc6dc5cd51b")
 
 function ReadBlog(){
-    let blogs = localStorage.getItem(tblBlog);
-    console.log(blogs);
+    GetBlogs();
 }
 
-function CreateBlog(){
-    let blogs = localStorage.getItem(tblBlog);
-    let lst = [];
-    if(blogs !== null){
-        lst = JSON.parse(blogs);
-    }
-
+function CreateBlog(title,author,content){
+    
+    let lst = GetBlogs();
     const requestData = {
         'id' : uuidv4(),
-        'title' : 'blog title',
-        'author' : 'blog author',
-        'content' : 'blog content'
+        'title' : title,
+        'author' : author,
+        'content' : content
     };
 
     lst.push(requestData);
     const jsonData = JSON.stringify(lst);
     localStorage.setItem(tblBlog,jsonData)
+    successMessage("Save Success.")
+    clearControl();
+}
+
+function EditBlog(id){
+    let lst = GetBlogs();
+    const items = lst.filter(x => x.id === id);
+    if(items.length == 0){
+        console.log("No Data Found...");
+        errorMessage("No Data Found...");
+        return;
+    }
+
+    item = items[0];
+    blogId = item.id
+    $("#txtTitle").val(item.title);
+    $("#txtAuthor").val(item.author);
+    $("#txtContent").val(item.content);
 }
 
 function UpdateBlog(id,title,author,content){
-    let blogs = localStorage.getItem(tblBlog);
-    let lst = [];
-    if(blogs !== null){
-        lst = JSON.parse(blogs);
-    }
+    let lst = GetBlogs();
     
     const items = lst.filter(x => x.id === id);
     if(items.length == 0){
         console.log("No Data Found...");
+        errorMessage("No Data Found...");
         return;
     }
 
@@ -50,15 +62,13 @@ function UpdateBlog(id,title,author,content){
 
     const jsonData = JSON.stringify(lst);
     localStorage.setItem(tblBlog,jsonData)
+    successMessage("Update Success.");
 }
 
 function DeleteBlog(id){
-    let blogs = localStorage.getItem(tblBlog);
-    let lst = [];
-    if(blogs !== null){
-        lst = JSON.parse(blogs);
-    }
-    
+    let result = confirm("Are your sure to delete?");
+    if(!result) return;
+    lst = GetBlogs(); 
     const items = lst.filter(x => x.id === id);
     if(items.length == 0){
         console.log("No Data Found...");
@@ -67,6 +77,8 @@ function DeleteBlog(id){
     lst = lst.filter(x => x.id !== id);
     const jsonData = JSON.stringify(lst);
     localStorage.setItem(tblBlog,jsonData)
+    successMessage("Delete Success.")
+    getBlogTable();
 }
 
 function uuidv4() {
@@ -74,3 +86,56 @@ function uuidv4() {
       (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
     );
   }
+
+  function GetBlogs(){
+    let blogs = localStorage.getItem(tblBlog);
+    let lst = [];
+    if(blogs !== null){
+        lst = JSON.parse(blogs);
+    }
+    return lst;
+}
+
+function successMessage(message){
+    alert(message)
+}
+
+function clearControl(){
+    $("#txtTitle").val('');
+    $("#txtAuthor").val('');
+    $("#txtContent").val('');
+}
+
+function getBlogTable(){
+    const lst = GetBlogs();
+    let count = 0;
+    let htmlRows = '';
+    lst.forEach(item => {
+        const htmlRow = `
+            <tr>
+                <td><input type="button" value="Edit" onclick="EditBlog('${item.id}')" class="btn btn-warning"></td>
+                <td><input type="button" value="Delete" onclick="DeleteBlog('${item.id}')" class="btn btn-danger"></td>
+                <td scope="row">${++count}</td>
+                <td>${item.title}</td>
+                <td>${item.author}</td>
+                <td>${item.content}</td>
+            </tr>
+        `;
+        htmlRows += htmlRow;
+    });
+    $("#tbody").html(htmlRows);
+}
+
+$("#btnSave").click(function(){
+    const title = $("#txtTitle").val();
+    const author = $("#txtAuthor").val();
+    const content = $("#txtContent").val();
+    if(blogId === null){
+        CreateBlog(title,author,content)
+    }
+    else{
+        UpdateBlog(blogId,title,author,content)
+        blogId = null;
+    }
+    getBlogTable();
+})
